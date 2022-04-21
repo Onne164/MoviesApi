08660d44c6e0ca911851ef236715f037a2ab65bd
Õnne Õuemaa <onne.ouemaa@tptlive.ee>
2022-04-21 13:49:29 +0300

    added Create btn

--- rest-client.js
+++ rest-client.js
@@ -3,7 +3,11 @@ const vue = Vue.createApp({
          return {
              movieInModal: {title: null},
              movies: [],
-             actors: []
+             actors: [],
+             myModel:false,
+             dynamicTitle:'Add Movie',
+             actionButton:'Insert',
+
          }
      },
      async created() {
@@ -14,6 +18,34 @@ const vue = Vue.createApp({
              this.movieInModal = await(await fetch(`http://localhost:8080/movies/${id}`)).json()
              let movieInfoModal = new bootstrap.Modal(document.getElementById('movieInfoModal'), {})
              movieInfoModal.show()
-         }
+         },
+         openModel: function(){
+            title = '';
+            year = '';
+            actors = [];
+            actionButton = "Insert";
+            dynamicTitle = "Add Movie";
+            this.myModel = true;
+           },
+           submitData:function(){
+            if(title != '' && year != '')
+            {
+             if(actionButton == 'Insert')
+             {
+              app.post('/movies', (req, res), {
+               action:'insert',
+               title:app.title, 
+               year:app.year,
+               actors: app.actors
+              }).then(function(response){
+               app.myModel = false;
+               app.fetchAllData();
+               app.title = '';
+               app.year = '';
+               alert(response.data.message);
+              });
+             }
+            }
+        }
      }
  }).mount('#app')
